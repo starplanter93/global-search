3c8e264bd18d7ee2fe130fb377d873f3b99bb07a
starplanter93 <starplanter93@gmail.com>
2024-01-14 16:22:31 +0900

    layout 설정

--- global-search-react/src/App.jsx
+++ global-search-react/src/App.jsx
@@ -1,32 +1,21 @@
-import { Link, Route, Routes, useNavigate } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Home from './pages/Home';
 import Search from './pages/Search';
 import Country from './pages/Country';
 import NotFound from './pages/NotFound';
+import Layout from './components/Layout';
 
 function App() {
-  const nav = useNavigate();
-  const onClick = () => {
-    nav('/search');
-  };
-
   return (
-    <>
+    <Layout>
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/search' element={<Search />} />
         <Route path='/country/:code' element={<Country />} />
         <Route path='*' element={<NotFound />} />
       </Routes>
-      <div>
-        <Link to={'/'}>Home</Link>
-        <Link to={'/'}>Search</Link>
-        <Link to={'/'}>Country</Link>
-
-        <button onClick={onClick}>서치페이지로 이동</button>
-      </div>
-    </>
+    </Layout>
   );
 }
 
