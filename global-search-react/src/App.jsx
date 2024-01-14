3fd9c730229275f3f4c0d658215c04a8d12c9fe4
starplanter93 <starplanter93@gmail.com>
2024-01-14 16:06:08 +0900

    query, params를 통한 동적경로 설정

--- global-search-react/src/App.jsx
+++ global-search-react/src/App.jsx
@@ -1,4 +1,4 @@
-import { Route, Routes } from 'react-router-dom';
+import { Link, Route, Routes, useNavigate } from 'react-router-dom';
 import './App.css';
 import Home from './pages/Home';
 import Search from './pages/Search';
@@ -6,13 +6,27 @@ import Country from './pages/Country';
 import NotFound from './pages/NotFound';
 
 function App() {
+  const nav = useNavigate();
+  const onClick = () => {
+    nav('/search');
+  };
+
   return (
-    <Routes>
-      <Route path='/' element={<Home />} />
-      <Route path='/search' element={<Search />} />
-      <Route path='/country' element={<Country />} />
-      <Route path='*' element={<NotFound />} />
-    </Routes>
+    <>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/search' element={<Search />} />
+        <Route path='/country/:code' element={<Country />} />
+        <Route path='*' element={<NotFound />} />
+      </Routes>
+      <div>
+        <Link to={'/'}>Home</Link>
+        <Link to={'/'}>Search</Link>
+        <Link to={'/'}>Country</Link>
+
+        <button onClick={onClick}>서치페이지로 이동</button>
+      </div>
+    </>
   );
 }
 
