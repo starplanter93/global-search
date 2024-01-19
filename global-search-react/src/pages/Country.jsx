1fb7829c37584991b4d24c75923a13dbfb2f1a93
starplanter93 <starplanter93@gmail.com>
2024-01-19 21:09:46 +0900

    country페이지 완료

--- global-search-react/src/pages/Country.jsx
+++ global-search-react/src/pages/Country.jsx
@@ -1,6 +1,7 @@
 import { useCallback, useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { fetchCountry } from '../api';
+import style from './Country.module.css';
 
 export default function Country() {
   const params = useParams();
@@ -15,5 +16,35 @@ export default function Country() {
     setInitData();
   }, [params.code, setInitData]);
 
-  return <div>Country : {params.code}</div>;
+  return (
+    <div className={style.container}>
+      <div className={style.header}>
+        <div className={style.commonName}>
+          {country.flagEmoji}&nbsp;{country.commonName}
+        </div>
+        <div className={style.officialName}>{country.officialName}</div>
+      </div>
+      <img
+        src={country.flagImg}
+        alt={`${country.commonName}의 국기 이미지입니다`}
+      />
+      <div className={style.body}>
+        <div>
+          <b>코드 :</b>&nbsp;{country.code}
+        </div>
+        <div>
+          <b>수도 :</b>&nbsp;{country.capital.join(', ')}
+        </div>
+        <div>
+          <b>지역 :</b>&nbsp;{country.region}
+        </div>
+        <div>
+          <b>지도 :</b>&nbsp;
+          <a target='_blank' rel='noreferrer' href={country.googleMapURL}>
+            {country.googleMapURL}
+          </a>
+        </div>
+      </div>
+    </div>
+  );
 }
